e557546d7447e6750e5824f7cb2060e0a655808e
VENKATA RAMANA PEDAVEGI <pedavegi72@gmail.com>
2026-08-18 22:47:35 +0530

    ✅ UWCOI20A - Rating 650 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-18

---
+++ codechef/easy/UWCOI20A---Rating-650/solution.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+
+		while (T-- > 0)
+		{
+			int N = sc.nextInt();
+
+			int max = 0;
+
+			for (int i = 0; i < N; i++)
+			{
+				int height = sc.nextInt();
+
+				if (height > max)
+				{
+					max = height;
+				}
+			}
+
+			System.out.println(max);
+		}
+
+		sc.close();
+
+	}
+}
