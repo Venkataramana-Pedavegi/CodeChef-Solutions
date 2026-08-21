489f58ffa81709fba4d7115d77a635dcc9929ebc
VENKATA RAMANA PEDAVEGI <pedavegi72@gmail.com>
2026-08-21 20:05:56 +0530

    ✅ LJAAS39 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-21

---
+++ codechef/medium/LJAAS39/solution.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String one = "Coding"; // Strings always go inside double quotes
+        String two = "Chaf";
+        two = two.replace('a','e'); // This is the correct statement to replace a character in a string.
+        System.out.println(one + " " + two);
+    }
+}
