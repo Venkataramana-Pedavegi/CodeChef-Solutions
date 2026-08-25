b7e1ef6003fe92bf1ecf29d2c3443aad664991e9
VENKATA RAMANA PEDAVEGI <pedavegi72@gmail.com>
2026-08-25 19:53:10 +0530

    ✅ JPRACMCQ3 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-25

---
+++ codechef/medium/JPRACMCQ3/solution.cpp
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int r=sc.nextInt();
+		int c=sc.nextInt();
+		int e=sc.nextInt();
+		System.out.println((r+e)*c);
+
+	}
+}
