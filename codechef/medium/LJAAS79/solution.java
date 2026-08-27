b42fca36c1d987ebe7574b11fb7b7681e6ee027b
VENKATA RAMANA PEDAVEGI <pedavegi72@gmail.com>
2026-08-27 19:27:18 +0530

    ✅ LJAAS79 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-27

---
+++ codechef/medium/LJAAS79/solution.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char ch = scanner.next().charAt(0);
+
+        switch(ch) {
+            case 'a': 
+                System.out.println("Vowel"); 
+                break;
+            case 'e': 
+                System.out.println("Vowel"); 
+                break;
+            case 'i': 
+                System.out.println("Vowel"); 
+                break;
+            case 'o': 
+                System.out.println("Vowel"); 
+                break;
+            case 'u': 
+                System.out.println("Vowel"); 
+                break;
+            default: 
+                System.out.println("Consonant");
+        }
+    }
+}
