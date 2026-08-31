18e5c8216d43e47865e7e974b5d5c8bf4a0bb389
VENKATA RAMANA PEDAVEGI <pedavegi72@gmail.com>
2026-08-31 15:44:13 +0530

    ✅ ABDIFF - Rating 347 | Easy | C++ | Runtime: N/A | Memory: N/A | 2026-08-31

---
+++ codechef/easy/ABDIFF---Rating-347/solution.cpp
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+				int blackCells = (n * n) / 2;
+		System.out.println(blackCells);
+
+		
+	}
+}
