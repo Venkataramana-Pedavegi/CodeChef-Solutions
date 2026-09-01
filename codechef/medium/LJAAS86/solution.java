b6531afe183b06f8de640275c9d8ee027402a0e9
VENKATA RAMANA PEDAVEGI <pedavegi72@gmail.com>
2026-09-01 22:43:40 +0530

    ✅ LJAAS86 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-01

---
+++ codechef/medium/LJAAS86/solution.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	    int number = 19; // Add missing semicolon
+        System.out.println(number);
+
+	}
+}
