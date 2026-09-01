8c690507cabfda5a57b4cb2e62fd0428c23227f1
VENKATA RAMANA PEDAVEGI <pedavegi72@gmail.com>
2026-09-01 22:44:13 +0530

    ✅ LJAAS88 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-01

---
+++ codechef/medium/LJAAS88/solution.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+        int n;
+        n = 5;
+
+        if (n % 2 == 0) 
+        {
+            System.out.println("true");
+        } 
+        else 
+        {
+            System.out.println("false");
+        }
+
+	}
+}
