341f5a7adc0455c6bb8d33ee2fdcb6e44c1bb8bd
VENKATA RAMANA PEDAVEGI <pedavegi72@gmail.com>
2026-09-02 20:14:14 +0530

    ✅ LJAAS110 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-02

--- codechef/medium/LJAAS110/solution.java
+++ codechef/medium/LJAAS110/solution.java
@@ -1,12 +1,24 @@
+import java.util.Scanner;
+
 class Codechef
 {
 	public static void main (String[] args) 
 	{
-		int num = 1;
-        while (num <= 5)
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        if(num==0)
         {
-           System.out.print(num * num + " ");
-           num++;
+            System.out.println(1);
         }
+        else{
+            int factorial = 1;
+            do {
+                factorial *= num;
+                num--;
+            } while (num > 0);
+    
+            System.out.println(factorial);
+        }
+        
 	}
 }
