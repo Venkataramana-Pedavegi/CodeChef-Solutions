c356a6550fdc7fc8f675b254c6ede169464147ea
VENKATA RAMANA PEDAVEGI <pedavegi72@gmail.com>
2026-09-03 20:41:59 +0530

    ✅ LJAAS130 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-03

---
+++ codechef/medium/LJAAS130/solution.java
@@ -0,0 +1,15 @@
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		int[] values = {2, 10, 12, 6, 15};
+        for (int x : values) 
+        {
+            if (x > 10) 
+            {
+                continue;
+            }
+            System.out.print(x * x + " ");
+        }        
+    }
+}
