a23d6a092036f6b89b14b20da161fc3c253528ce
VENKATA RAMANA PEDAVEGI <pedavegi72@gmail.com>
2026-09-03 20:40:19 +0530

    ✅ LJAAS129 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-03

---
+++ codechef/medium/LJAAS129/solution.java
@@ -0,0 +1,15 @@
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		int[] numbers = {2, 4, 8, 12, 8};
+        for (int i = 0; i < numbers.length; i++) 
+        {
+            if (numbers[i] == 8) 
+            {
+               System.out.println(i);
+               break;
+            }
+        }
+	}
+}
