53efb5fa27eae3f2261c53e92b5238f21fe3dff1
VENKATA RAMANA PEDAVEGI <pedavegi72@gmail.com>
2026-09-05 05:43:20 +0530

    ✅ SQUATS - Rating 249 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-05

---
+++ codechef/easy/SQUATS---Rating-249/solution.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0)
+		{
+    		int x = sc.nextInt();
+    		System.out.println(x*15);
+		}
+		
+	}
+}
