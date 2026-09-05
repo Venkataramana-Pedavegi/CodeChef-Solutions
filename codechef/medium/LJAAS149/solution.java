681ec1dfdcc7f0d848463c4ca851284a4f144aa6
VENKATA RAMANA PEDAVEGI <pedavegi72@gmail.com>
2026-09-05 22:33:13 +0530

    ✅ LJAAS149 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-05

---
+++ codechef/medium/LJAAS149/solution.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        while (t-- > 0) {
+            int N = scanner.nextInt();
+            System.out.println(N + 1);
+        }
+    }
+}
