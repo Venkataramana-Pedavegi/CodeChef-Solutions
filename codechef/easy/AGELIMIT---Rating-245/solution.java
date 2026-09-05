072bbfff6b19018fda612da4ac33b5a62bbc3253
VENKATA RAMANA PEDAVEGI <pedavegi72@gmail.com>
2026-09-05 05:42:02 +0530

    ✅ AGELIMIT - Rating 245 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-05

---
+++ codechef/easy/AGELIMIT---Rating-245/solution.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0)
+		{
+    		int x = sc.nextInt();
+    		int y = sc.nextInt();
+    		int a = sc.nextInt();
+    		if(a>=x && a<y){
+    		    System.out.println("yes");
+    		}else{
+    		    System.out.println("no");
+    		}
+		}
+		
+	}
+}
