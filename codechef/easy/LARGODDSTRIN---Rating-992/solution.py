d666a16c4688745bfb622ba522669f43a6a45c81
VENKATA RAMANA PEDAVEGI <venkataramanapedavegi9@gmail.com>
2026-09-12 21:02:45 +0530

    ✅ LARGODDSTRIN - Rating 992 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-09-12

---
+++ codechef/easy/LARGODDSTRIN---Rating-992/solution.py
@@ -0,0 +1,14 @@
+t = int(input())
+
+while t > 0:
+    s = input()
+    x = int(s[0:2])
+    y = int(s[3:5])
+    
+    if x <= 12 and y <= 12:
+        print("BOTH")
+    elif y <= 12:
+        print("DD/MM/YYYY")
+    else:
+        print("MM/DD/YYYY")
+    t -= 1
