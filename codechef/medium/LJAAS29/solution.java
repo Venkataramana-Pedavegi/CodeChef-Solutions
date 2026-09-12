3e7591a6704aec48bb077915648928fc1b3faca7
VENKATA RAMANA PEDAVEGI <venkataramanapedavegi9@gmail.com>
2026-09-12 20:41:28 +0530

    ✅ LJAAS29 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-12

--- codechef/medium/LJAAS29/solution.java
+++ codechef/medium/LJAAS29/solution.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int x=20;
-        int y=6;
-        System.out.println(x/y);
-        
+        int x = 20;
+        int y = 6;
+        System.out.println(x / y);
     }
 }
