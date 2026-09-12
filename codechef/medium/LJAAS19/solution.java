1148ad718c4e58442d1047a0d41e85b80ba2990e
VENKATA RAMANA PEDAVEGI <venkataramanapedavegi9@gmail.com>
2026-09-12 20:38:51 +0530

    ✅ LJAAS19 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-12

--- codechef/medium/LJAAS19/solution.java
+++ codechef/medium/LJAAS19/solution.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("I");
-                System.out.println("love");
-        System.out.println("CodeChef");
-
+        System.out.println("I love Codechef");
     }
 }
