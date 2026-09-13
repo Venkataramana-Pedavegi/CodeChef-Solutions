253e30d41197a18537770a78942d5077de34d17d
VENKATA RAMANA PEDAVEGI <venkataramanapedavegi9@gmail.com>
2026-09-13 22:25:45 +0530

    ✅ Search a Character in a String | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-13

--- gfg/easy/search-a-character-in-a-string/solution.java
+++ gfg/easy/search-a-character-in-a-string/solution.java
@@ -1,7 +1,5 @@
 class Solution {
-    // Function to search for a character in the string
     public int searchCharacter(String s, char ch) {
-        // code here
         return s.indexOf(ch);
 
 
