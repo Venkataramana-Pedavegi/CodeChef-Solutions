250fdc060ccf5f20781cf89b27b7253fb738e2a2
VENKATA RAMANA PEDAVEGI <venkataramanapedavegi9@gmail.com>
2026-09-16 11:08:49 +0530

    ✅ MATTREVERSE | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-16

---
+++ codechef/medium/MATTREVERSE/solution.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        int[][] matrix = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = N - 1; i >= 0; i--) {
+            for (int j = 0; j < M; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+
+	}
+}
