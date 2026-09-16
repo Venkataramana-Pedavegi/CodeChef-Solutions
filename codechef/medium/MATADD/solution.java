1fc4165c1e8f8f5ef37f19f36a126cd6c0aa23f0
VENKATA RAMANA PEDAVEGI <venkataramanapedavegi9@gmail.com>
2026-09-16 11:06:00 +0530

    ✅ MATADD | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-16

--- codechef/medium/MATADD/solution.java
+++ codechef/medium/MATADD/solution.java
@@ -8,27 +8,24 @@ class Codechef
 	{
         Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt(); // rows
-        int M = sc.nextInt(); // columns
+        int N = sc.nextInt(); 
+        int M = sc.nextInt(); 
 
         int[][] A = new int[N][M];
         int[][] B = new int[N][M];
 
-        // Read first matrix
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 A[i][j] = sc.nextInt();
             }
         }
 
-        // Read second matrix
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 B[i][j] = sc.nextInt();
             }
         }
 
-        // Add and print matrices
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 System.out.print((A[i][j] + B[i][j]) + " ");
