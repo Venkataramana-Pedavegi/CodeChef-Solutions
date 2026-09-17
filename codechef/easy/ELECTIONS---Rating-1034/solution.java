3de9b319db1819c49876b00017dedeb1b36e66fd
VENKATA RAMANA PEDAVEGI <venkataramanapedavegi9@gmail.com>
2026-09-17 21:51:16 +0530

    ✅ ELECTIONS - Rating 1034 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-17

---
+++ codechef/easy/ELECTIONS---Rating-1034/solution.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int XA = sc.nextInt();
+            int XB = sc.nextInt();
+            int XC = sc.nextInt();
+
+            if (XA > 50) {
+                System.out.println("A");
+            } 
+            else if (XB > 50) {
+                System.out.println("B");
+            } 
+            else if (XC > 50) {
+                System.out.println("C");
+            } 
+            else {
+                System.out.println("NOTA");
+            }
+        }
+
+        sc.close();
+    }
+}
