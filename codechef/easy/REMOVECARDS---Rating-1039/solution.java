6ca4f2ec202880c45b3d95bef0ba1e28a70fc74b
VENKATA RAMANA PEDAVEGI <venkataramanapedavegi9@gmail.com>
2026-09-17 21:55:36 +0530

    ✅ REMOVECARDS - Rating 1039 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-17

---
+++ codechef/easy/REMOVECARDS---Rating-1039/solution.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+
+            int N = sc.nextInt();
+
+            int[] freq = new int[11];
+
+            for (int i = 0; i < N; i++) {
+                int x = sc.nextInt();
+                freq[x]++;
+            }
+
+            int maxFreq = 0;
+
+            for (int i = 1; i <= 10; i++) {
+                maxFreq = Math.max(maxFreq, freq[i]);
+            }
+
+            System.out.println(N - maxFreq);
+        }
+
+        sc.close();
+    }
+}
