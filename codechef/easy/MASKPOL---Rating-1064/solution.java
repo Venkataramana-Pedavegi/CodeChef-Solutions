a6d1bb578b044b3a6a88a7d3c3f59c4ad616f726
VENKATA RAMANA PEDAVEGI <venkataramanapedavegi9@gmail.com>
2026-09-18 20:53:03 +0530

    ✅ MASKPOL - Rating 1064 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-18

--- codechef/easy/MASKPOL---Rating-1064/solution.java
+++ codechef/easy/MASKPOL---Rating-1064/solution.java
@@ -2,26 +2,17 @@ import java.util.*;
 
 class Main {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
         int T = sc.nextInt();
 
         while (T-- > 0) {
+            int N = sc.nextInt();
+            int A = sc.nextInt();
 
-            int X = sc.nextInt();
-            int Y = sc.nextInt();
-            int Z = sc.nextInt();
+            int uninfected = N - A;
 
-            if (X >= Y) {
-                System.out.println("PIZZA");
-            }
-            else if (X >= Z) {
-                System.out.println("BURGER");
-            }
-            else {
-                System.out.println("NOTHING");
-            }
+            System.out.println(Math.min(A, uninfected));
         }
 
         sc.close();
