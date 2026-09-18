b5eaba5725ade46df66e81adfaec5c9ab1e93da9
VENKATA RAMANA PEDAVEGI <venkataramanapedavegi9@gmail.com>
2026-09-18 20:52:04 +0530

    ✅ PIZZA_BURGER - Rating 1064 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-18

---
+++ codechef/easy/PIZZA_BURGER---Rating-1064/solution.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+
+            int X = sc.nextInt();
+            int Y = sc.nextInt();
+            int Z = sc.nextInt();
+
+            if (X >= Y) {
+                System.out.println("PIZZA");
+            }
+            else if (X >= Z) {
+                System.out.println("BURGER");
+            }
+            else {
+                System.out.println("NOTHING");
+            }
+        }
+
+        sc.close();
+    }
+}
