9bc353ff06cb91803125e41da566eea0469cd2da
VENKATA RAMANA PEDAVEGI <venkataramanapedavegi9@gmail.com>
2026-09-18 20:57:04 +0530

    ✅ HEADBOB - Rating 1065 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-18

--- codechef/easy/HEADBOB---Rating-1065/solution.java
+++ codechef/easy/HEADBOB---Rating-1065/solution.java
@@ -8,21 +8,16 @@ class Main {
 
         while (T-- > 0) {
             int N = sc.nextInt();
+            String s = sc.next();
 
-            int oddCount = 0;
-
-            for (int i = 0; i < N; i++) {
-                int x = sc.nextInt();
-
-                if (x % 2 != 0) {
-                    oddCount++;
-                }
-            }
-
-            if (oddCount >= 2 && oddCount % 2 == 0) {
-                System.out.println("YES");
-            } else {
-                System.out.println("NO");
+            if (s.contains("I")) {
+                System.out.println("INDIAN");
+            } 
+            else if (s.contains("Y")) {
+                System.out.println("NOT INDIAN");
+            } 
+            else {
+                System.out.println("NOT SURE");
             }
         }
 
