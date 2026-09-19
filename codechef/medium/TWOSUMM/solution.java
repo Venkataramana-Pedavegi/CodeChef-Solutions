233015fe59fe8124708870bfb3b5bafa60f7f5f1
VENKATA RAMANA PEDAVEGI <venkataramanapedavegi9@gmail.com>
2026-09-19 21:48:49 +0530

    ✅ TWOSUMM | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-19

--- codechef/medium/TWOSUMM/solution.java
+++ codechef/medium/TWOSUMM/solution.java
@@ -1,3 +1,17 @@
 public static int[] findPair(int[] nums, int target) {
-    // Write your code here
+
+    HashMap<Integer, Integer> map = new HashMap<>();
+
+    for (int i = 0; i < nums.length; i++) {
+
+        int needed = target - nums[i];
+
+        if (map.containsKey(needed)) {
+            return new int[]{map.get(needed), i};
+        }
+
+        map.put(nums[i], i);
+    }
+
+    return new int[]{-1, -1};
 }
