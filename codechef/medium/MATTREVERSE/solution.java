362719f73ece2f406843f63faeac64ca6fe216ab
VENKATA RAMANA PEDAVEGI <venkataramanapedavegi9@gmail.com>
2026-09-21 22:00:23 +0530

    ✅ MATTREVERSE | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-21

--- codechef/medium/MATTREVERSE/solution.java
+++ codechef/medium/MATTREVERSE/solution.java
@@ -1,32 +1,39 @@
 import java.util.*;
-import java.lang.*;
-import java.io.*;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		Scanner sc = new Scanner(System.in);
+class Main {
+    public static void main(String[] args) {
 
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
 
-        int[][] matrix = new int[N][M];
+        int n = sc.nextInt();
+        int m = sc.nextInt();
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
+        int[][] matrix = new int[n][m];
+
+        // Input matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 matrix[i][j] = sc.nextInt();
             }
         }
 
-        for (int i = N - 1; i >= 0; i--) {
-            for (int j = 0; j < M; j++) {
-                System.out.print(matrix[i][j] + " ");
+        // Zig-zag traversal
+        for (int i = 0; i < n; i++) {
+
+            if (i % 2 == 0) {
+
+                // Even row → left to right
+                for (int j = 0; j < m; j++) {
+                    System.out.print(matrix[i][j] + " ");
+                }
+
+            } else {
+
+                // Odd row → right to left
+                for (int j = m - 1; j >= 0; j--) {
+                    System.out.print(matrix[i][j] + " ");
+                }
             }
-            System.out.println();
         }
-
-        sc.close();
-
-	}
+    }
 }
