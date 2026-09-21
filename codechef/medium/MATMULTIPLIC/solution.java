8628fee14c1c57ec45bcce15a99fb992c2ffe54e
VENKATA RAMANA PEDAVEGI <venkataramanapedavegi9@gmail.com>
2026-09-21 22:07:39 +0530

    ✅ MATMULTIPLIC | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-21

--- codechef/medium/MATMULTIPLIC/solution.java
+++ codechef/medium/MATMULTIPLIC/solution.java
@@ -5,18 +5,52 @@ class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+        // Dimensions of first matrix
+        int M = sc.nextInt();
+        int N = sc.nextInt();
 
-        if ((n * m) % 2 != 0) {
-            System.out.println(-1);
-            return;
+        int[][] A = new int[M][N];
+
+        // Input first matrix
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                A[i][j] = sc.nextInt();
+            }
         }
 
-        for (int i = 0; i < n; i++) {
+        // Dimensions of second matrix
+        int N2 = sc.nextInt();
+        int P = sc.nextInt();
 
-            for (int j = 0; j < m; j++) {
-                System.out.print("1 ");
+        int[][] B = new int[N2][P];
+
+        // Input second matrix
+        for (int i = 0; i < N2; i++) {
+            for (int j = 0; j < P; j++) {
+                B[i][j] = sc.nextInt();
+            }
+        }
+
+        // Result matrix: M × P
+        int[][] C = new int[M][P];
+
+        // Matrix multiplication
+        for (int i = 0; i < M; i++) {
+
+            for (int j = 0; j < P; j++) {
+
+                for (int k = 0; k < N; k++) {
+
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+
+        // Print result
+        for (int i = 0; i < M; i++) {
+
+            for (int j = 0; j < P; j++) {
+                System.out.print(C[i][j] + " ");
             }
 
             System.out.println();
