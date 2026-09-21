16a60d3d6282b7b84b1e3a512634f4c7f9541370
VENKATA RAMANA PEDAVEGI <venkataramanapedavegi9@gmail.com>
2026-09-21 22:06:41 +0530

    ✅ DSCPPAS263 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-21

--- codechef/medium/DSCPPAS263/solution.java
+++ codechef/medium/DSCPPAS263/solution.java
@@ -8,36 +8,18 @@ class Main {
         int n = sc.nextInt();
         int m = sc.nextInt();
 
-        int[][] matrix = new int[n][m];
+        if ((n * m) % 2 != 0) {
+            System.out.println(-1);
+            return;
+        }
 
         for (int i = 0; i < n; i++) {
+
             for (int j = 0; j < m; j++) {
-                matrix[i][j] = sc.nextInt();
+                System.out.print("1 ");
             }
+
+            System.out.println();
         }
-
-        int row = 0;
-        int col = m - 1;
-        int count = 0;
-
-        while (row < n && col >= 0) {
-
-            if (matrix[row][col] < 0) {
-
-                // Everything below this element in this column
-                // is also negative.
-                count += n - row;
-
-                col--;
-
-            } else {
-
-                // Current element is non-negative.
-                // Move down to find negatives.
-                row++;
-            }
-        }
-
-        System.out.println(count);
     }
 }
