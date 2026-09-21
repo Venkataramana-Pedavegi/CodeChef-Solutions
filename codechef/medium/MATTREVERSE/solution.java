cea570e344b758e3ca6bb702b98a0c8f49f43f15
VENKATA RAMANA PEDAVEGI <venkataramanapedavegi9@gmail.com>
2026-09-21 22:01:34 +0530

    ✅ MATTREVERSE | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-21

--- codechef/medium/MATTREVERSE/solution.java
+++ codechef/medium/MATTREVERSE/solution.java
@@ -10,30 +10,21 @@ class Main {
 
         int[][] matrix = new int[n][m];
 
-        // Input matrix
+        // Input
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 matrix[i][j] = sc.nextInt();
             }
         }
 
-        // Zig-zag traversal
-        for (int i = 0; i < n; i++) {
+        // Print rows from bottom to top
+        for (int i = n - 1; i >= 0; i--) {
 
-            if (i % 2 == 0) {
-
-                // Even row → left to right
-                for (int j = 0; j < m; j++) {
-                    System.out.print(matrix[i][j] + " ");
-                }
-
-            } else {
-
-                // Odd row → right to left
-                for (int j = m - 1; j >= 0; j--) {
-                    System.out.print(matrix[i][j] + " ");
-                }
+            for (int j = 0; j < m; j++) {
+                System.out.print(matrix[i][j] + " ");
             }
+
+            System.out.println();
         }
     }
 }
