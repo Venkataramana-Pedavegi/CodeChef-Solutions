86dbabc7b1abe94d926cab8b2cd8581a91010f33
VENKATA RAMANA PEDAVEGI <venkataramanapedavegi9@gmail.com>
2026-09-23 20:23:03 +0530

    ✅ ECHOECHO | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-23

--- codechef/medium/ECHOECHO/solution.java
+++ codechef/medium/ECHOECHO/solution.java
@@ -4,9 +4,22 @@ import java.io.*;
 
 class Codechef
 {
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
 
-	}
+        String s = sc.next();
+
+        if (s.charAt(0) == s.charAt(2) &&
+            s.charAt(1) == s.charAt(3))
+        {
+            System.out.println("Yes");
+        }
+        else
+        {
+            System.out.println("No");
+        }
+
+        sc.close();
+    }
 }
