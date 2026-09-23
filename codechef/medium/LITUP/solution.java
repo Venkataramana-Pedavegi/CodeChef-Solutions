66cc48bb9f7629a9907d4d28346194b433e157dd
VENKATA RAMANA PEDAVEGI <venkataramanapedavegi9@gmail.com>
2026-09-23 20:26:02 +0530

    ✅ LITUP | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-23

--- codechef/medium/LITUP/solution.java
+++ codechef/medium/LITUP/solution.java
@@ -4,9 +4,64 @@ import java.io.*;
 
 class Codechef
 {
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
 
-	}
+        int T = sc.nextInt();
+
+        while (T-- > 0)
+        {
+            int N = sc.nextInt();
+            int K = sc.nextInt();
+
+            int[] C = new int[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                C[i] = sc.nextInt();
+            }
+
+            int minCost = Integer.MAX_VALUE;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = i + 1; j < N; j++)
+                {
+                    boolean allLit = true;
+                    for (int x = 0; x < N; x++)
+                    {
+                        boolean light1 = Math.abs(i - x) <= K;
+                        boolean light2 = Math.abs(j - x) <= K;
+
+                        if (!light1 && !light2)
+                        {
+                            allLit = false;
+                            break;
+                        }
+                    }
+
+                    if (allLit)
+                    {
+                        int cost = C[i] + C[j];
+
+                        if (cost < minCost)
+                        {
+                            minCost = cost;
+                        }
+                    }
+                }
+            }
+
+            if (minCost == Integer.MAX_VALUE)
+            {
+                System.out.println(-1);
+            }
+            else
+            {
+                System.out.println(minCost);
+            }
+        }
+
+        sc.close();
+    }
 }
